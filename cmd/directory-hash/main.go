29a8bac2ea7dfa453e40e71267ec7cad00c50fae
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-08-19 18:04:30 +0100

    Consolidate packages and add comments

--- cmd/directory-hash/main.go
+++ cmd/directory-hash/main.go
@@ -3,25 +3,36 @@ package main
 import (
 	"fmt"
 
-	dir "golang.org/x/mod/sumdb/dirhash"
+	dir "github.com/ministryofjustice/cloud-platform-directory-hash/pkg/hashdir"
+	create "golang.org/x/mod/sumdb/dirhash"
 )
 
 const fileName string = ".checksum"
 const base string = "namespaces/live-1.cloud-platform.service.justice.gov.uk"
 
 func main() {
-	// DefaultHash is the default hash function used in new go.sum entries.
-	var DefaultHash dir.Hash = dir.Hash1
+	// DefaultHash is the default hash function used to hash a directory.
+	var DefaultHash create.Hash = create.Hash1
 
-	original, namespace := read.readChecksum(fileName)
+	// Introspects a file found in the root of the cloud-platform-environments.
+	prevHash, namespace := dir.ReadChecksum(fileName)
 
+	// Sets up the relative path of a namespace.
 	nsDir := base + "/" + namespace
 
-	hashNs, _ := dir.HashDir(nsDir, namespace, DefaultHash)
+	// Creates a new sha256 hash of a namespace.
+	newHash, err := create.HashDir(nsDir, namespace, DefaultHash)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	if check.hashesMatch(original, hashNs) && check.singleNamespace(namespace) {
+	// Compares two hashes and ensures only a single namespace has been modified.
+	// If both conditions have been met a file will be created for a downstream GitHub
+	// action. If the conditions fail, a message will be printed. The script deliberately
+	// returns exit code 0 regardless of pass or fail to allow GitHub actions to report success.
+	if dir.HashesMatch(prevHash, newHash) && dir.SingleNamespace(namespace, base) {
 		fmt.Println("Checksums match. Approve PR.")
-		err := create.createArtifact("pass")
+		err := dir.CreateArtifact("pass")
 		if err != nil {
 			fmt.Println(err)
 		}
